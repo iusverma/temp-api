3979c67ffdd35972b46265d732747528f4c86ee3
Ayush Verma <verma.ius@gmail.com>
2020-08-04 19:10:08 +1000

    Added list of generics.

--- src/main/java/com/ayush/temp/api/controller/ApiController.java
+++ src/main/java/com/ayush/temp/api/controller/ApiController.java
@@ -1,6 +1,10 @@
 package com.ayush.temp.api.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,12 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+
+	@Value("${generics}")
+	String [] generics;
 	
 	@RequestMapping(value="/generics", method=RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<String> fetchGenerics(){
+	public ResponseEntity<String> fetchGenerics(
+			@RequestBody String body,
+			HttpServletRequest request){
 		ResponseEntity<String> re = null;
 		System.out.println("Reached Controller.");
+		System.out.println("Request"+body);
+		System.out.println("Generics loaded: " + generics.length);
+
 		return re;
 	}
 }
