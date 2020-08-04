95d11fc67998270b3404c58c74dc8a920a95e993
Ayush Verma <verma.ius@gmail.com>
2020-08-04 20:51:57 +1000

    Completed with request and response.

--- src/main/java/com/ayush/temp/api/controller/ApiController.java
+++ src/main/java/com/ayush/temp/api/controller/ApiController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ayush.temp.api.utils.YamlParser;
 import com.ayush.temp.objects.Generic;
+import com.ayush.temp.objects.Response;
 
 @RestController
 @RequestMapping("/api")
@@ -30,22 +31,39 @@ public class ApiController {
 
 	@RequestMapping(value="/generics", method=RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<String> fetchGenerics(
+	public Response fetchGenerics(
 			@RequestBody String body,
 			HttpServletRequest request){
-		ResponseEntity<String> re = null;
+		Response re = null;
 		List<Generic> ret = new ArrayList<>();
 		System.out.println("Reached Controller.");
 		System.out.println("Request"+body);
 		System.out.println("Generics loaded: " + generics.length);
 
-		for(int i=0;i<generics.length;i++) {
-			ret.addAll(yamlParser.getData(generics[i]));
+		if(body.contains("{}")
+				||body.contains("[{}]")
+			    ||body.contains("[]")
+				||body.length()<10){
+			// No request load all data
+			for(int i=0;i<generics.length;i++) {
+				ret.addAll(yamlParser.getData(generics[i]));
+			}
+		}else {
+			// Selective code fetch
+			for(int i=0;i<generics.length;i++) {
+				if(body.contains(generics[i])) {
+					ret.addAll(yamlParser.getData(generics[i]));
+				}
+			}
 		}
 		if(!CollectionUtils.isEmpty(ret)) {
+			re = new Response();
+			List<Generic> generics = new ArrayList<>();
 			for(Generic gen: ret) {
 				System.out.println("Code: "+gen.getCode() +", Name: "+gen.getName());
 			}
+			generics.addAll(ret);
+			re.setGenerics(generics);
 		}
 		return re;
 	}
---
+++ src/main/java/com/ayush/temp/objects/Response.java
@@ -0,0 +1,16 @@
+package com.ayush.temp.objects;
+
+import java.util.List;
+
+public class Response {
+	List<Generic> generics;
+
+	public List<Generic> getGenerics() {
+		return generics;
+	}
+
+	public void setGenerics(List<Generic> generics) {
+		this.generics = generics;
+	}
+	
+}
